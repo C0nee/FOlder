0cf44df77d26c77f6a0867a8e3d71e89b091ebe1
K <you@example.com>
2022-11-16 13:59:43 +0100

    Commit13.59

--- Klasa.php
+++ Klasa.php
@@ -11,4 +11,12 @@ public function _construct(string $login, string $password){
     $this->login = $login;
     $this-> password = $password;
 }}
+public function register(){
+    $passwordhash = password_hash($this->$password, PASSWORD_ARGON2I);
+    $q = INSERT INTO user VALUES(null, ?, ?, ?, ?);
+    $db = new mysqli('localhost', 'root', "", 'user');
+    $pq = $db-> prepare($q);
+    $pq -> bind_param('ssss', $this->login,$passwordhash, $this->FirstName, $this->LastName);
+    $pq -> execute();
+}
 ?>
