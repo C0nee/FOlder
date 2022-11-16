4a52792b059220fe3db4df507cc26aaaf11aad1d
K <you@example.com>
2022-11-16 14:53:48 +0100

    NowyCommit 3

--- Klasa.php
+++ Klasa.php
@@ -1,41 +1,51 @@
 <?php
+class user {
+    private int $id;
+    private string $login;
+    private string $password;
+    private string $FirstName;
+    private string $LastName;
 
-
-class user{
-private int $id;
-private string $login;
-private string $password;
-private string $FirstName;
-private string $Lastname;
-
-
-
-
-
-    public function __construct(string $Login, string $password){
-
-    $this->login = $login = "a";
-    $this->password = $password;
+    public function __construct(string $login, string $password) {
+        $this->login = $login;
+        $this->password = $password;
         $this->FirstName = "";
         $this->LastName = "";
+    }
 
+    public function register() : bool {
+        $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
+        $q = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $db = new mysqli('localhost', 'root', '', 'Form');
+        $pq = $db->prepare($q); 
+        $pq->bind_param('ssss', $this->login, $passwordHash, 
+                                            $this->FirstName, $this->LastName);
+        $result = $pq->execute();
+        return $result;
+    }
+
+    public function login() : bool {
+        $q = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $db = new mysqli('localhost', 'root', '', 'Form');
+        $pq = $db->prepare($q); 
+        $pq->bind_param('s', $this->login);
+        $pq->execute();
+        $result = $pq->get_result();
+        if($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $passwordHash = $row['password'];
+            if(password_verify($this->password, $passwordHash)) {
+                $this->id = $row['id'];
+                $this->FirstName = $row['FirstName'];
+                $this->LastName = $row['LastName'];
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
 }
-
-
-public function register(){
-
-$passwordhash = password_hash($this->password, PASSWORD_ARGON2I);
-$q = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
-$db = new mysqli('localhost', 'root', '', 'Form');
-$pq = $db-> prepare($q);
-$pq -> bind_param('ssss', $this->login, $passwordhash, $this->FirstName, $this->LastName);
-$pq -> execute(); var_dump($pq);
-}
-
-
-}
-
-
-
 ?>
--- tak.php
+++ tak.php
@@ -1,8 +1,19 @@
 <?php
 require_once('Klasa.php');
-$user = new user('jkowalski', 'haslo');
-$user -> register();
 
-echo '<pre>';
-var_dump($user);
+$user = new user('jkowalski', 'tajneHasło');
+/*
+if($user->register()) {
+    echo "Zarejestrowano poprawnie";
+} else {
+    echo "Błąd rejestracji użytkownika";
+}
+*/
+
+if($user->login()) {
+    echo "Zalogowano poprawnie";
+} else {
+    echo "Błędny login lub hasło";
+}
+
 ?>
