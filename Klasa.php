9af3acb67fe0cd366c8de8f536b52796742a07d2
K <you@example.com>
2022-11-23 13:11:00 +0100

    Commit 23.11 13 10

--- Klasa.php
+++ Klasa.php
@@ -1,7 +1,7 @@
 <?php
 class user {
-    //commit
     private int $id;
+    private mysqli $db;
     private string $login;
     private string $password;
     private string $FirstName;
@@ -12,26 +12,26 @@ class user {
         $this->password = $password;
         $this->FirstName = "";
         $this->LastName = "";
+        global $db;
+        $this->db = &$db;
     }
 
     public function register() : bool {
         $passwordHash = password_hash($this->password, PASSWORD_ARGON2I);
-        $q = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
-        $db = new mysqli('localhost', 'root', '', 'form');
-        $pq = $db->prepare($q); 
-        $pq->bind_param('ssss', $this->login, $passwordHash, 
+        $query = "INSERT INTO user VALUES (NULL, ?, ?, ?, ?)";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('ssss', $this->login, $passwordHash, 
                                             $this->FirstName, $this->LastName);
-        $result = $pq->execute();
+        $result = $preparedQuery->execute();
         return $result;
     }
 
     public function login() : bool {
-        $q = "SELECT * FROM user WHERE login = ? LIMIT 1";
-        $db = new mysqli('localhost', 'root', '', 'form');
-        $pq = $db->prepare($q); 
-        $pq->bind_param('s', $this->login);
-        $pq->execute();
-        $result = $pq->get_result();
+        $query = "SELECT * FROM user WHERE login = ? LIMIT 1";
+        $preparedQuery = $this->db->prepare($query); 
+        $preparedQuery->bind_param('s', $this->login);
+        $preparedQuery->execute();
+        $result = $preparedQuery->get_result();
         if($result->num_rows == 1) {
             $row = $result->fetch_assoc();
             $passwordHash = $row['password'];
@@ -47,6 +47,14 @@ class user {
             return false;
         }
     }
-
+    public function setFirstName(string $FirstName) {
+        $this->FirstName = $FirstName;
+    }
+    public function setLastName(string $LastName) {
+        $this->LastName = $LastName;
+    }
+    public function getName() : string {
+        return $this->FirstName . " " . $this->LastName;
+    }
 }
 ?>
